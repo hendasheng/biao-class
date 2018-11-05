4c780b26c0740a4814226894f3d4589b9a61b4a1
niu <lnlaoniu@gmail.com>
2018-11-05 17:04:58 +0800

    JavaScript Test

---
+++ pracitce/038-js/main.js
@@ -0,0 +1,30 @@
+;(function() {
+    'use styicy';
+
+    let a, b;
+
+    a = 100;
+    b = 200;
+
+    console.log(a);
+    console.log('+');
+    console.log(b);
+    console.log('=');
+
+    console.log(a + b);
+
+    const C = '常量';
+    const PI = 3.1415;
+    console.log(C);
+    console.log(PI * a);
+
+    const DD = 122;
+
+    console.log(DD + ' ' + C);
+
+    const N = 'niu';
+    const age = 25;
+
+    console.log(N + ' ' + '今年' + ' ' + age + ' ' + '岁了');
+    
+})();
