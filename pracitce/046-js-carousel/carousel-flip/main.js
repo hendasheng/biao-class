9d5f2135c431394e6b3ced85486cfd07b05ed761
niu <lnlaoniu@gmail.com>
2018-11-18 17:36:14 +0800

    carousel-flip

---
+++ pracitce/046-js-carousel/carousel-flip/main.js
@@ -0,0 +1,22 @@
+; (function () {
+    'use strict';
+
+    const parent = document.getElementById('box');
+    const item = parent.querySelectorAll('.item');
+
+    let lastIndex = item.length - 1;
+    let currentIndex = 0;
+
+    boot();
+    function boot() {
+
+    }
+    
+    setInterval(() => {
+        
+    }, 1000);
+
+
+
+
+})();
