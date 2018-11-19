d28f2b5883aa3e9dd6258c4d04e3d382c99b0e8a
niu <lnlaoniu@gmail.com>
2018-11-19 12:38:29 +0800

    carousel-opacity

---
+++ pracitce/046-js-carousel/carousel-opacity/main.js
@@ -0,0 +1,62 @@
+; (function () {
+    'use strict';
+
+    const parent = document.getElementById('box');
+    const slides = parent.querySelectorAll('.item');
+
+    let lastIndex = slides.length - 1;
+    let current = 0;
+
+    boot();
+    function boot() {
+        show(slides[current]);
+        setInterval(() => {
+            loop();
+        }, 1000);
+    }
+
+    function loop() {
+        current++;
+
+        if(current >= slides.length)
+            current = 0;
+        
+        let prev = getPrev();
+        let next = getNext();
+
+        hide(prev);
+        show(next);
+    }
+
+    /**
+     *  如果 
+     *  
+     * @returns
+     */
+    function getPrev() {
+        if (current === 0)
+            return slides[lastIndex];
+        return slides[current - 1];
+    }
+
+    function getNext() {
+        if (current < slides.length)
+            return slides[current];
+        return slides[0]
+    }
+
+    function hide(el) {
+        if (!el)
+            return;
+        el.style.opacity = 0;
+    }
+    function show(el) {
+        if (!el)
+            return;
+        el.style.opacity = 1;
+    }
+
+
+
+
+})();
