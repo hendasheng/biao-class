5f89efc5becb8e505318f8045dd4900129a5c812
niu <lnlaoniu@gmail.com>
2018-12-10 21:38:11 +0800

    user manager

--- pracitce/055-user-manager/js/main.js
+++ pracitce/055-user-manager/js/main.js
@@ -23,6 +23,13 @@
     let elTable = document.getElementById('user-table');
     let elTbody = elTable.tBodies[0];
 
+    let inputs = {
+        index: elForm.querySelector('[name=index]'),
+        username: elForm.querySelector('[name=username]'),
+        email: elForm.querySelector('[name=email]'),
+        balance: elForm.querySelector('[name=balance]'),
+    };
+
     boot();
 
     function boot() {
@@ -34,29 +41,61 @@
         elForm.addEventListener('submit', e => {
             e.preventDefault();
             let row = {};
-            row.username = elForm.querySelector('[name=username]').value;
-            row.email = elForm.querySelector('[name=email]').value;
-            row.balance = elForm.querySelector('[name=balance]').value;
 
-            users.push(row);
-            render();
+            let index = inputs.index.value;
+
+            row.username = inputs.username.value;
+            row.email = inputs.email.value;
+            row.balance = inputs.balance.value;
+
+            if (index)
+                users[index] = row;
+            else
+                users.push(row);
+
             elForm.reset();
+            render();
 
         });
     }
 
     function render() {
         elTbody.innerHTML = '';
-        users.forEach(user => {
+        users.forEach((user, i) => {
+            if (!user)
+                return;
+
             let tr = document.createElement('tr');
             tr.innerHTML = `
                 <td>${user.username}</td>
                 <td>${user.email}</td>
                 <td>${user.balance}</td>
+                <td class="text-right operations">
+                    <button class="fill">更新</button>
+                    <button class="delete">删除</button>
+                </td>
             `;
 
+            let q = tr.querySelector.bind(tr);
+
+            q('.operations').addEventListener('click', e => {
+                let klass = e.target.classList;
+                if (klass.contains('delete')) {
+                    users[i] = null;
+                    tr.remove();
+                    console.log(users);
+                }
+
+                if (klass.contains('fill')) {
+                    inputs.index.value = i;
+                    inputs.username.value = user.username;
+                    inputs.email.value = user.email;
+                    inputs.balance.value = user.balance;
+                }
+            })
+
+
             elTbody.appendChild(tr);
-            
         });
     }
 
