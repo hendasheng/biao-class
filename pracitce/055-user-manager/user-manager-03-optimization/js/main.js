84186623de9240f49eb5a06c657871ef37a92fad
niu <lnlaoniu@gmail.com>
2018-12-21 11:37:23 +0800

    user manager - optimization

--- pracitce/055-user-manager/user-manager-03-optimization/js/main.js
+++ pracitce/055-user-manager/user-manager-03-optimization/js/main.js
@@ -23,6 +23,8 @@
     let elTable = document.getElementById('user-table');
     let elTbody = elTable.querySelector('tbody');
     let inputs = elForm.querySelectorAll('[name]');
+    // 用于判断点击 更新 时，被更新数据的索引值 
+    let elIndex = document.querySelector('[name=index]');
 
     boot();
 
@@ -45,11 +47,24 @@
                 // 保存到 row
                 row[it.name] = it.value;
             });
-            // 把 row 中的数据推到 users 数组中
-            users.push(row);
+
+            // 如果记录索引组建中有值
+            // 如 <input type="number" name="index" value="1">
+            if (elIndex.value) {
+                // 就更新数据
+                users[elIndex.value] = row;
+            } else {
+                // 否则把新的数据推到 users 数组中
+                users.push(row);
+            }
+
+            // 每次提交后清空 记录索引值的组件
+            elIndex.value = '';
+
             // 每次提交后清空 form 中的数据
             elForm.reset();
-            // 渲染 table
+
+            // 但数据发生改变，就重新渲染 table
             renderTbale();
         });
     }
@@ -61,9 +76,10 @@
         // 循环 users - 用户数据
         // it 代表每一条用户数据，i 代表每条数据的索引值
         users.forEach((it, i) => {
-            // 如果用户不存在就跳过
+            // 如果当前用户（tr）不存在就跳过
             // 因为删除用户时会将数据设为null
-            if (!users)
+            // 避免 tr 中只出现 '更新' '删除' 两个按钮
+            if (!it)
                 return;
 
             // 初始化组装字符串
@@ -87,6 +103,7 @@
                         <button class="delete">删除</button>
                     </td>`;
 
+
             // 把拼接好的字符串存到 tr 中
             tr.innerHTML = html;
 
@@ -96,10 +113,12 @@
                 let el = e.target;
                 // 如果点击的是 fill - 更新
                 if (el.classList.contains('fill')) {
+                    // 就给 index 输入组件填入相应索引值
+                    elIndex.value = i;
                     // 循环当前这条用户数据
-                    for(let key in it) {
+                    for (let key in it) {
                         // 找到相对应的 input - 输入组件
-                        let input = document.querySelector(`[name=${key}]`);
+                        let input = elForm.querySelector(`[name=${key}]`);
                         // 在 input 中填入数据
                         input.value = it[key];
                     };
@@ -108,8 +127,9 @@
                 if (el.classList.contains('delete')) {
                     tr.remove();
                     users[i] = null;
-                    // console.log(users);
+                    console.log(users);
                 };
+                console.log(i)
             });
 
             // 把拼装好的 tr 插入到 tbody 中
