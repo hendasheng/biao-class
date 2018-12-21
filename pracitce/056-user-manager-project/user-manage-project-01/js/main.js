e44e015aefad1c4f435374eb24fcc812ffa43d8f
niu <lnlaoniu@gmail.com>
2018-12-21 16:50:58 +0800

    user manager - 运用前面写过的插件 - bug

--- pracitce/056-user-manager-project/user-manage-project-01/js/main.js
+++ pracitce/056-user-manager-project/user-manage-project-01/js/main.js
@@ -2,6 +2,7 @@
     'use strict';
 
     let bf, bt;
+    let form = document.querySelector('form');
 
     let structure = {
         username: '用户名',
@@ -22,27 +23,40 @@
         },
     ];
 
-    let actions = {
-        Delete(tr, i) {
-            tr.remove();
-            list[i] = null;
-        },
-        Updata(tr, i) {
 
-            bf.setData(list[i]);
-        }
-    };
 
     boot();
 
     function boot() {
+        perpareForm();
+        perpareTable();
+    }
+
+
+    function perpareForm() {
         bf = biaoForm('form', onSubmit);
+    }
+
+    function perpareTable() {
+        let actions = {
+            Delete(tr, i) {
+                tr.remove();
+                list[i] = null;
+            },
+            Updata(tr, i) {
+                form.querySelector('[name=index]').value = i;
+                bf.setData(list[i]);
+            },
+        };
 
         bt = biaoTable('table', structure, list, actions);
     }
 
-    function onSubmit(data) {
-        list.push(data);
+    function onSubmit(row) {
+        if (!row.index && row.index !== 0)
+            list.push(row);
+        list[row.index] = row;
+
         bt.render();
     }
 
