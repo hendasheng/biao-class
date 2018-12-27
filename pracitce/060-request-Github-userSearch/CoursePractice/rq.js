57e3ef46511969ed062352d89841bb5615a12b39
niu <lnlaoniu@gmail.com>
2018-12-27 17:54:27 +0800

    Github User Search

--- pracitce/060-request-Github-userSearch/CoursePractice/rq.js
+++ pracitce/060-request-Github-userSearch/CoursePractice/rq.js
@@ -1,15 +1,28 @@
 ; (function () {
     'use strict';
 
+
     window.rq = { get, post };
 
+    /**
+     * 取数据 - 从服务器获取数据
+     * @param url - 服务器地址
+     */
     function get(url, onSuccess, onError) {
         send(url, 'get', null, onSuccess, onError);
-    };
+    }
 
+
+    /**
+     * 存数据 - 往服务器发数据
+     *
+     * @param {} url - 接收数据的地址
+     * @param {*} data - 发送的数据
+     */
     function post(url, data) {
         send(url, 'post', data, onSuccess, onError);
     };
+
 
     function send(url, type, data, onSuccess, onError) {
         let http = new XMLHttpRequest();
@@ -17,9 +30,9 @@
         http.send();
 
         http.addEventListener('load', () => {
+            // 解析数据
             let data = JSON.parse(http.responseText);
             onSuccess && onSuccess(data);
-        })
-    }
-
+        });
+    };
 })();
