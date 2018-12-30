5cc9594758c995b8cdfb5ddf708e91542b3ade08
niu <lnlaoniu@gmail.com>
2018-12-31 00:25:29 +0800

    Pagination - plugin

--- pracitce/061-Pagination-plugin/main.js
+++ pracitce/061-Pagination-plugin/main.js
@@ -1,19 +1,12 @@
 biaoPage.boot({
+    // 选择器
     selector: '.footer',
+    // 数量
     amount: 50,
+    // 限制
     limit: 10,
     currentPage: 2,
-    onChange(page,state) {
-        console.log(page);
-    },
-});
-
-
-biaoPage.boot({
-    selector: '#a',
-    amount: 100,
-    limit: 15,
-    currentPage: 3,
+    // 更改
     onChange(page,state) {
         console.log(page);
     },
@@ -21,12 +14,24 @@ biaoPage.boot({
 
 
 
-biaoPage.boot({
-    selector: '#b',
-    amount: 100,
-    limit: 20,
-    currentPage: 1,
-    onChange(page,state) {
-        console.log(page);
-    },
-});
+// biaoPage.boot({
+//     selector: '#a',
+//     amount: 100,
+//     limit: 15,
+//     currentPage: 3,
+//     onChange(page,state) {
+//         console.log(page);
+//     },
+// });
+
+
+
+// biaoPage.boot({
+//     selector: '#b',
+//     amount: 100,
+//     limit: 20,
+//     currentPage: 1,
+//     onChange(page,state) {
+//         console.log(page);
+//     },
+// });
