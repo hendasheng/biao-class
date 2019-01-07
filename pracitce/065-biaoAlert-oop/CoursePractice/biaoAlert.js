dba9f8cd00539e53a2e55d7eee7bbed4f69c62d1
niu <lnlaoniu@gmail.com>
2019-01-07 17:57:41 +0800

    biaoAlert-oop - CoursePractice

--- pracitce/065-biaoAlert-oop/CoursePractice/biaoAlert.js
+++ pracitce/065-biaoAlert-oop/CoursePractice/biaoAlert.js
@@ -1,105 +1,122 @@
 ; (function () {
     'use strict';
 
+    /**
+     * 穿
+     *
+     * @class Alert
+     */
     class Alert {
-        constructor(title, { desc, onClick, onOpen, onClose, type = 'info', timeout = 3000, containerClass = 'biao-alert-container', clickToClose = true } = {}) {
+        constructor(title, { desc, onClick, onOpen, onClose, type = 'info', timeout = 3000, containerClass = 'biao-alert-container', clickToClose = true }) {
+            // 加载设置
             this.title = title;
             this.desc = desc;
             this.type = type;
             this.timeout = timeout;
-            this.onClick = onClick;
-            this.onOpen = onOpen;
-            this.onClose = onClose;
             this.containerClass = containerClass;
             this.clickToClose = clickToClose;
+            this.onOpen = onOpen;
+            this.onClick = onClick;
+            this.onClose = onClose;
 
-            this.containrer = null;
+            this.container = null;
 
-            this.prepareEnv();
+            this.prepare();
             this.render();
             this.open();
-            this.bindClick();
+            this.bindEvents();
+
         }
 
-
-        prepareEnv() {
-            this.container =
-                this.getContainer();
-
-            // 有就用
-            if (this.getContainer())
+        /**
+         * 准备环境
+         * 所有提醒都在一个容器内 - containerClass
+         * @returns
+         * @memberof Alert
+         */
+        prepare() {
+            this.container = document.querySelector('.' + this.containerClass);
+            // 如果有就跳过
+            if (this.container)
                 return;
 
-            // 没有就造一个
+            // 没有就早一个
             let container = this.container = document.createElement('div');
             container.classList.add(this.containerClass);
             document.body.appendChild(container);
         }
 
-
-        getContainer() {
-            return document.querySelector('.' + this.containerClass);
-        }
-
-
+        /**
+         * 渲染
+         * @returns
+         * @memberof Alert
+         */
         render() {
+            // 创建包含每条提醒的容器
             let el = document.createElement('div');
+            // 设置类名
             el.classList.add('biao-alert');
-
-            // 给不同的类型添加不同的类
-            // 实现不同类型不同的颜色
+            // 讲 type 设置为 类名，已实现不同类型不同颜色的样式
             el.classList.add(this.type);
-            // el.hidden = true;
             el.innerHTML = `
-                    <div class="inner">
-                    <div class="title">${this.title}</div>
-                    ${this.desc ? `<div class="desc">${this.desc}</div>` : ''}
-                    </div>
-                `;
-            document.body.appendChild(el);
-
-            // 把 biao-alert(每个提醒) 缓存到 this 中
+                            <div class="inner">
+                            <div class="title">${this.title}</div>
+                            ${this.desc ? `<div class="desc">${this.desc}</div>` : ''}
+                            </div>
+                            `;
+            // 把包含每条挺行的容器缓存到 this 中， 方便以后调用
             this.el = el;
         }
 
-
+        /**
+         * 显示提醒
+         * @memberof Alert
+         */
         open() {
-            // 如果有回调就叫 回调函数
-            this.onOpen && this.onOpen();
+            // 如果有回调就叫一下 回调
+            this.onOpen && this.onOpen(this);
             this.container.appendChild(this.el);
 
-            // console.log(this.el);
-            // 如果没有期限，就一直显示
+            // 如果 timeout 为 false 则跳过
             if (!this.timeout)
                 return;
 
-            // 如果有期限就按人家说的办
-            setTimeout(e => {
-                // 到指定时间隐藏提醒
-                this.close(this);
+            // 每 timeout 执行一次 close
+            setTimeout($ => {
+                this.close();
             }, this.timeout);
         }
 
-
+        /**
+         * 隐藏提醒
+         * @memberof Alert
+         */
         close() {
-            this.onClose && this.onClose();
-            this.el.hidden = true;
+            // 如果有回调就叫一下 回调
+            this.onClose && this.onClose(this);
+            this.el.style.opacity = 0;
         }
 
-
-        bindClick() {
+        /**
+         * 当被点击时
+         *
+         * @memberof Alert
+         */
+        bindEvents() {
             this.el.addEventListener('click', e => {
-                // clickToClose 为 true 则表示执行在点击时隐藏
-                if (this.clickToClose)
-                    close(this);
-
-                // 如果有回调，就叫回调函数
+                // 如果有回调就叫一下 回调                
                 this.onClick && this.onClick();
-
-            });
+                // 如果有 clickToclose 为 true
+                if (this.clickToClose)
+                    // 就执行 close
+                    this.close();
+            })
         }
+
     }
+
     window.Alert = Alert;
+
 })();
 
 
