34a45b08ce4ec5981386289c99b8da6178f9e0f3
niu <lnlaoniu@gmail.com>
2019-01-08 17:55:23 +0800

    To do list

--- pracitce/062-ToDo/Exercise/complete-01/js/main.js
+++ pracitce/062-ToDo/Exercise/complete-01/js/main.js
@@ -19,35 +19,52 @@
     }
 
     /**
-     * 
+     * 获取数据
      */
     function read() {
         api('todo/read', null, data => {
+            // 把数据储存到全文变量中
             $list = data.data;
+            // 获取数据后渲染 item
             render();
         });
     }
 
+    /**
+     * 绑定事件
+     */
     function bindEvents() {
         todoForm.addEventListener('submit', e => {
             e.preventDefault();
+            // 取得输入框中输入的内容
             let title = input.value;
+            // 如果有 id 说明此时为更新，则执行 update
             if (currentId)
                 update({ id: currentId, title });
+            // 否则执行创建 item
             else
                 create({ title: title });
         });
     }
 
 
+    /**
+     * 在数据库中创建一条 item
+     * @param {Object} row 
+     */
     function create(row) {
         api('todo/create', row, result => {
+            // 创建后，重新获取数据
             if (result)
                 read();
+            // 清空输入框
             todoForm.reset();
         })
     }
 
+    /**
+     *  渲染 item
+     */
     function render() {
         list.innerHTML = '';
         $list.forEach(it => {
@@ -74,6 +91,7 @@
                 setCompleted(it.id, checkbox.checked);
             });
 
+            // 当点击 更新 或 删除 时
             operation.addEventListener('click', e => {
                 let target = e.target;
                 if (target.classList.contains('fill')) {
