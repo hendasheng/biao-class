e35496c46e4b28d839511ccd521ccfab4e5f0ee5
niu <lnlaoniu@gmail.com>
2019-01-14 15:20:54 +0800

    biaoAlert - Exercise 03

--- pracitce/064-biaoAlert/Exercise/03/biaoAlert.js
+++ pracitce/064-biaoAlert/Exercise/03/biaoAlert.js
@@ -5,21 +5,25 @@
 
     let container;
 
+    // 默认配置
     const defaultConfig = {
-        type: 'info',
-        timeout: 2000,
-        containerClass: 'biao-alert-container',
-        clickToClose: true,
+        type: 'info', // 类型
+        timeout: 2000,  // 在 timeout 后，启动 xx
+        containerClass: 'biao-alert-container', // 容器类名
+        clickToClose: true, // 有点击关闭事件
     }
 
+    // 启动
     function boot(title, config) {
+        // 合并用户配置和默认配置
         config = { ...defaultConfig, ...config, title };
         prepare(config);
         render(config);
         openRemind(config);
         bindEvents(config);
     }
-
+    
+    // 准备
     function prepare(config) {
         container = document.querySelector('.' + config.containerClass);
         if (container)
@@ -30,6 +34,7 @@
         document.body.appendChild(container);
     }
 
+    // 渲染消息 list
     function render(config) {
         let el = document.createElement('div');
         el.classList.add('biao-alert');
@@ -43,23 +48,37 @@
         config.el = el;
     }
 
+    /**
+     * 打开 提醒
+     * @param {Object} config 
+     */
     function openRemind(config) {
+        // 如果有 onOpen 就执行
         config.onOpen && config.onOpen(config);
         container.appendChild(config.el);
 
         if (!config.timeout)
             return;
 
+        // 参照 timeout 关闭提醒
         setTimeout($ => {
             closeRemind(config);
         }, config.timeout);
     }
 
+    /**
+     * 关闭 提醒
+     * @param {Object} config 
+     */
     function closeRemind(config) {
         config.onClose && config.onClose(config);
         config.el.style.opacity = 0;
     }
 
+    /**
+     * 绑定点击事件
+     * @param {Object} config 
+     */
     function bindEvents(config) {
         config.el.addEventListener('click', e => {
             config.onClick && config.onClick(config);
