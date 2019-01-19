f97f286fccc9b277cf8292d5d4eaf48c05fa5b83
niu <lnlaoniu@gmail.com>
2019-01-19 23:55:57 +0800

    webpack - style-loader / css-loader

---
+++ pracitce/067-webpack/Exercise/05/webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+}
