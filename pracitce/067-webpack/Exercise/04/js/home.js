a8a91d87ac45c729ab58ba4529f9b1232815dc83
niu <lnlaoniu@gmail.com>
2019-01-19 17:57:21 +0800

    webpack

---
+++ pracitce/067-webpack/Exercise/04/js/home.js
@@ -0,0 +1,8 @@
+
+import { open } from './base';
+
+if (open) {
+    document.body.innerHTML = `
+                            <a href="signup.html">注册</a>
+                            `;
+}
---
+++ pracitce/067-webpack/Exercise/04/js/signup.js
@@ -0,0 +1,12 @@
+
+import { open } from './base';
+
+if (open) {
+    document.body.innerHTML = `
+                                <h1>欢迎入坑</h1>
+                                `;
+} else {
+    document.body.innerHTML = `
+                                <h1>暂不接受注册</h1>
+                                `;
+};
