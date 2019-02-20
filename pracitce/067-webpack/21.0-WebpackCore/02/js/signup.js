22e857b5b77f0e1a14eb62f8d1c9f76ac74be396
niu <lnlaoniu@gmail.com>
2019-02-20 17:03:37 +0800

    biaoyansu 21.0 Webpack 核心 -02

---
+++ pracitce/067-webpack/21.0-WebpackCore/02/js/signup.js
@@ -0,0 +1,7 @@
+import { open } from './base';
+
+if (open) {
+    document.body.innerHTML = `<h1>欢迎入坑：）</h1>`;
+} else {
+    document.body.innerHTML = `<h1>暂不开放注册</h1>`;
+}
---
+++ pracitce/067-webpack/21.0-WebpackCore/02/webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    entry: {
+        home: './js/home.js',
+        signup: './js/signup',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: __dirname + '/dist',
+    }
+
+}
