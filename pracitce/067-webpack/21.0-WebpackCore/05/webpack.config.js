c21dce93ef0a48c443d8637a75bbd605880780ef
niu <lnlaoniu@gmail.com>
2019-02-21 10:40:23 +0800

    21.0 Webpack 核心 -05 style-loader%css-loader

---
+++ pracitce/067-webpack/21.0-WebpackCore/05/webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    }
+                ],
+            },
+        ],
+    },
+
+};
