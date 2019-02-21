12a7f103dca8796504770225baa3d2b5b1905c9b
niu <lnlaoniu@gmail.com>
2019-02-21 10:11:45 +0800

    21.0 Webpack 核心 -04 loader

--- pracitce/067-webpack/21.0-WebpackCore/04/js/index.js
+++ pracitce/067-webpack/21.0-WebpackCore/04/js/index.js
@@ -1,5 +1,6 @@
 
 import { debug } from './base.js';
-import './css/base.css';
+import '../css/base.css';
+import '../css/base2.css';
 
 console.log('debug:', debug);
--- pracitce/067-webpack/21.0-WebpackCore/04/webpack.config.js
+++ pracitce/067-webpack/21.0-WebpackCore/04/webpack.config.js
@@ -5,9 +5,11 @@ module.exports = {
         path: __dirname + '/dist'
     },
     module: {
-        rules: {
-            test: /\.css$/, // 如果是 .css 文件
-            use: ['style-loader', 'css-loader'] // 则执行 css-loader / style-loader，从右到左执行
-        }
+        rules: [
+            {
+                test: /\.css$/, // 如果是 .css 文件
+                use: ['style-loader', 'css-loader'] // 则执行 css-loader / style-loader，从右到左执行
+            }
+        ]
     }
 }
