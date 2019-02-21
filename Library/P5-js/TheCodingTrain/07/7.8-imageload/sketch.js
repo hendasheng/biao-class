4f21a3b8bbe7d6b0c946a60a49c5541795b5c1c5
niu <lnlaoniu@gmail.com>
2019-02-21 15:03:17 +0800

    P5-js loadImage

--- Library/P5-js/TheCodingTrain/07/7.8-imageload/sketch.js
+++ Library/P5-js/TheCodingTrain/07/7.8-imageload/sketch.js
@@ -5,9 +5,9 @@ let kittens = [];
 
 function preload() {
     flower = loadImage('kittens/flower.png');
-      for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) {
         kittens[i] = loadImage(`kittens/kitten${i}.jpg`);
-      }
+    }
 }
 
 function setup() {
@@ -15,9 +15,8 @@ function setup() {
     for (let i = 0; i < 10; i++) {
         let x = random(width);
         let y = random(height);
-        let r = random(50, 150);
-        // let kitten = random(kittens);
-        let b = new Bubble(x, y, r);
+        let r = random(50, 100);
+        let b = new Bubble(x, y, r, flower);
         bubbles.push(b);
     }
 }
@@ -27,7 +26,6 @@ function mousePressed() {
         bubbles[i].clicked(mouseX, mouseY);
     }
 }
-
 
 function draw() {
     background(0);
@@ -42,15 +40,14 @@ class Bubble {
         this.x = x;
         this.y = y;
         this.r = r;
-        this.kitten = random(kittens);
+        this.kitten = img;
 
     }
 
     clicked(px, py) {
-        //let d = dist(px, py, this.x, this.y);
-        //if (d < this.r) {
+
         if (px > this.x && px < this.x + this.r && py > this.y && py < this.y + this.r) {
-            this.kitten = flower; //random(kittens);
+            this.kitten = random(kittens);
         }
     }
 
@@ -60,7 +57,7 @@ class Bubble {
     }
 
     show() {
-        image(flower, this.x, this.y, this.r, this.r);
+        image(this.kitten, this.x, this.y, this.r, this.r);
         // stroke(255);
         // strokeWeight(4);
         // fill(this.brightness, 125);
