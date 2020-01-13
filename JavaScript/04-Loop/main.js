1226c3bda9aa3479e8666af97b26ca5bab6e8a4a
niu <lnlaoniu@gmail.com>
2020-01-13 18:04:44 +0800

    04-Loop

---
+++ JavaScript/04-Loop/main.js
@@ -0,0 +1,44 @@
+; (function () {
+    'use strict';
+
+    // let i = 0;
+    // while (i < 3) {
+    //     console.log(i);
+    //     i++;
+    // }
+
+    // for (let i = 0; i < 3; i++) {
+    //     console.log(i);
+    // }
+
+    // let event = true;
+    // let current = 0;
+    // while (event) {
+    //     console.log('继续执行');
+    //     current++;
+    //     if (current >= 100) break;
+    // }
+
+    // let current = 0;
+    // while (current < 100) {
+    //     console.log('正在执行第' + current + '次');
+    //     current++;
+    // }
+
+    // 跳过 continue
+    // let a = [1, 2, 4, 5, 'haha', 6];
+    // for (let i = 0; i < a.length; i++) {
+    //     let it = a[i];
+    //     if (it === 'haha') continue;
+    //     // console.log('字符串');
+    //     console.log(it);
+    // }
+
+    // let a = [1, 2, 4, 5, 'haha', 6];
+    // for (let i = 0; i < a.length; i++) {
+    //     let it = a[i];
+    //     if(typeof(it) != 'number') continue;
+    //     console.log(it);
+    // }
+
+})();
