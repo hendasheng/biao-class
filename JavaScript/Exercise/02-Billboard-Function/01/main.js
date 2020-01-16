0e827e4d0d35ce639cb26157cbdbc3f178b38bb6
niu <lnlaoniu@gmail.com>
2020-01-16 19:02:13 +0800

    01

---
+++ JavaScript/Exercise/02-Billboard-Function/01/main.js
@@ -0,0 +1,37 @@
+; (function () {
+    'use strict';
+
+    const board = document.querySelector('.board');
+    const inner = board.querySelector('.inner');
+
+    let count = 1;
+
+    boot();
+
+    function boot() {
+        inner.innerText = '已有 ' + count + ' 人领取';
+        setInterval(() => {
+            if (isVisible()) {
+                onDisplay(false);
+            } else {
+                onDisplay(true);
+                count++;
+                inner.innerText = '已有 ' + count + ' 人领取';
+            }
+        }, 500);
+    }
+
+    function isVisible() {
+        return getComputedStyle(board).opacity == '1';
+    }
+
+    function onDisplay(check) {
+        // true ? board.style.opacity = 1: board.style.opacity = 0;
+        if(check) {
+            board.style.opacity = 1;
+        } else {
+            board.style.opacity = 0;
+        }
+    }
+    
+})();
