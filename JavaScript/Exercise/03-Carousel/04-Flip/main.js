94dc39197ace1714295696e33031cb6b9d10f754
niu <lnlaoniu@gmail.com>
2020-02-05 15:25:19 +0800

    03_Carousel -04-Flip

--- JavaScript/Exercise/03-Carousel/04-Flip/main.js
+++ JavaScript/Exercise/03-Carousel/04-Flip/main.js
@@ -1,15 +1,19 @@
 ; (function () {
     'use strict';
 
-    /**
-     * 图片轮播
+    /** Slide -> 图片 X轴 轮播
      * ’当前张‘ / ‘上一张’ / ’下一张‘ 分别做不同的动作
      * ‘上一张’ 沿 X 轴向左滑动（-width)
      * ’当前张‘ 设置位置在视图中心
      * ’下一张‘ 设置位置在试图中心右侧，准备向左滑动 (+width)
-     * 
      * X 轴 - position
      * Z 轴 - zIndex
+     */
+
+    /** Fade -> 图片渐隐渐显
+     * 默认所有 item 为隐藏
+     * '上一张' 隐藏, zIndex = 1；
+     * '当前张' 显示, zIndex = 2;
      */
 
     const container = document.querySelector('.container');
@@ -22,14 +26,44 @@
     let currentPage;
     let nextPage;
 
-    boot();
-    function boot() {
+    // 默认配置
+    let config = {
+        mode: 'slide',
+        interval: 1000,
+    }
+
+    boot({
+        mode: 'fade',
+    });
+
+    function boot(custom) {
+        // 合并对象，用 custom 合并 config，生成新的对象
+        config = Object.assign({}, config, custom);
+
+        switch (config.mode) {
+            case 'slide':
+                slide();
+                break;
+            case 'fade':
+                hideAll();
+                fade();
+                break;
+        }
 
         setInterval(() => {
             increment();
-            slider();
 
-        }, 1000)
+            switch (config.mode) {
+                case 'slide':
+                    slide();
+                    break;
+                case 'fade':
+                    hideAll();
+                    fade();
+                    break;
+            }
+
+        }, config.interval);
     }
 
     function increment() {
@@ -57,7 +91,7 @@
             return items[0];
     }
 
-    function slider() {
+    function slide() {
         prevPage = getPrev();
         currentPage = getCurrent();
         nextPage = getNext();
@@ -72,7 +106,21 @@
         nextPage.style.zIndex = 0;
     }
 
+    function fade() {
+        prevPage = getPrev();
+        currentPage = getCurrent();
 
+        prevPage.style.opacity = 0;
+        prevPage.style.zIndex = 1;
 
+        currentPage.style.opacity = 1;
+        currentPage.style.zIndex = 2;
+    }
+
+    function hideAll() {
+        items.forEach(it => {
+            it.style.opacity = 0;
+        })
+    }
 
 })();
