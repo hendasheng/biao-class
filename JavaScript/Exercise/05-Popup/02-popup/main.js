859ce6a289d0a33e8d29b1e6be7d0af00341c45a
niu <lnlaoniu@gmail.com>
2020-02-08 22:27:47 +0800

    popup

--- JavaScript/Exercise/05-Popup/02-popup/main.js
+++ JavaScript/Exercise/05-Popup/02-popup/main.js
@@ -7,9 +7,12 @@
      * 
      * 绑定打开
      * 显示 popup 并且重置 popup 位置
+     * 当浏览器窗口尺寸变化时，重置 popup 位置
      * 
      * 绑定关闭
      * 隐藏 popup
+     * 
+     * 设置 popup 位置
      * 
      */
 
@@ -28,8 +31,11 @@
     function bindOpen() {
         trigger.addEventListener('click', () => {
             setViseble(true);
-            rePosition();
+            rePosition('left', -150, 0);
         });
+        window.addEventListener('resize', () => {
+            rePosition('left', -150, 0);
+        })
     }
 
     function bindClose() {
@@ -42,8 +48,12 @@
         })
     }
 
-    function setViseble(show = false) {
+    function setViseble(show) {
         popup.hidden = mask.hidden = !show;
+        if (show)
+            mask.style.opacity = popup.style.opacity = 1;
+        else
+            mask.style.opacity = popup.style.opacity = 0;
     }
 
     function initPopup() {
@@ -54,16 +64,21 @@
         mask.hidden = popup.hidden = true;
     }
 
-    function rePosition() {
+    /**
+     * 
+     * @param {*} position center | top | left | right
+     * @param {*} yOffset 
+     * @param {*} xOffset 
+     */
+    function rePosition(position = 'center', yOffset, xOffset) {
         let windowWidth = window.innerWidth;
         let windowHeight = window.innerHeight;
 
         let width = popup.offsetWidth;
         let height = popup.offsetHeight;
 
-        popup.style.left = windowWidth / 2 - width / 2 + 'px';
-        popup.style.top = windowHeight / 2 - height / 2 + 'px';
+        popup.style.left = windowWidth / 2 - width / 2 + xOffset + 'px';
+        popup.style.top = windowHeight / 2 - height / 2 + yOffset + 'px';
     }
-
 
 })();
